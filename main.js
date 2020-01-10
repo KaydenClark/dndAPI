0528367be76af02c5ad0cc9921d48cbe0608f3e9
Kayden Clark <kaydenclark725@gmail.com>
2020-01-09 18:49:07 -0700

    updating api to express

--- main.js
+++ main.js
@@ -1,103 +1,14 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
 const app = express()
-const port = process.env.PORT || 3000
-const monstersList = require('./dnd_5e/monsters.json')
+const port = process.env.PORT || 5000
 
-let idCounter = 1;
-// The "Database"
-let entities = [
-    {
-        number: "0",
-        name: "Name",
-        meta: "Size, Alignment",
-        armorClass: "AC (Type)",
-        hitPoints: "HP (HitDice)",
-        speed: "Speed",
-        str: "STR",
-        str_mod: "(str_mod)",
-        dex: "DEX",
-        dex_mod: "(dex_mod)",
-        con: "CON",
-        con_mod: "(con_mod)",
-        int: "INT",
-        int_mod: "(int_mod)",
-        wis: "WIS",
-        wis_mod: "(wis_mod)",
-        cha: "CHA",
-        cha_mod: "(cha_mod)",
-        savingThrows: "throw1 (mod), throw2 (mod)",
-        skills: "skill (mod)",
-        senses: "passive (mod), other",
-        languages: "language1, language2",
-        challenge: "rate (XP)",
-        traits: "<p><em><strong>trait</strong></em> Description </p>",
-        actions: "<p><em><strong>action</strong></em> Description </p>",
-        legendaryActions: "<p><em><strong>trait</strong></em> Description </p>",
-        img_url: "url"
-      },
-]
+app.use(require('./routes'))
 
-app.use(express.json())
-app.use(cors())
-
-app.get('/', (req, res) => {
-    res.send(entities)
+app.use((req, res) => {
+    res.status(404)
+        .send('Unknown Request');
 });
 
-app.get('/monstersList', (req, res) => {
-    res.send(monstersList)
-});
-
-app.get('/monstersList/:name', (req, res) => {
-    const results = monstersList.filter((monster) => monster.name == req.params.name)
-    res.send(results)
-});
-
-// req.body Parsing
-// Validate the input
-// Push the input into the "Database"
-// Send a Response
-app.post('/', (req, res) => {
-    const entity = req.body
-    entity.id = idCounter
-    entities.push(entity)
-    idCounter++
-    res.send(entity)
+app.listen(port, () =>{
+    console.log(`Express listening on port ${port}`)
 })
-
-app.put('/monsters/:id', (req, res) => {
-    const updatedMonster = req.body
-    monsters.forEach((monster, index) => {
-        if(monster.id == req.params.id){
-            updatedMonster.id = parseInt(req.params.id)
-            monsters[index] = updatedMonster
-        }
-    })
-    res.send(updatedMonster)
-})
-
-app.patch('/monsters/:id', (req, res) => {
-    let result = 'Nothing was Updated'
-    monsters.forEach((monster, index) => {
-        if(monster.id == req.params.id){
-            // String Array of Request Body Properties Names
-            const bodyKeys = Object.keys(req.body)
-            bodyKeys.forEach(propName => {
-                // Computed Property Names
-                monsters[index][propName] = req.body[propName]
-                result = monsters[index]
-            })
-        }
-    })
-    res.send(result)
-})
-
-app.delete('/monsters/:id', (req, res) => {
-    const updatedContacts = monsters.filter((monster) => monster.id != req.params.id)
-    monsters = updatedContacts
-    res.send(`Deleted monster with ID ${req.params.id}`)
-})
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors')
+const router = express.Router();
+
+router.use(cors())
+router.use(express.json())
+
+router.use('/lists', require('./lists/listsAPI'))
+
+router.get('/test', (req, res) =>{
+    res.send('Hello World')
+})
+
+module.exports = router
