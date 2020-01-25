79902174a431bdb094c918f5857c9e4da4d10662
Kayden Clark <kaydenclark725@gmail.com>
2020-01-24 21:42:20 -0500

    added a read to characters

--- routes/character/character.js
+++ routes/character/character.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const {readCharacters} = require('../../DataAccessLayer/character/readCharacters')
 const {createCharacter} = require('../../DataAccessLayer/character/createCharacter')
 
 router.get('/', async (req, res) => {
-    res.send('Characters API')
+    res.send("playerData")
+})
+
+router.get('/allPlayerData', async (req, res) => {
+    characterList = await readCharacters()
+    res.send(characterList)
 })
 
 router.post('/', async (req, res) => {
